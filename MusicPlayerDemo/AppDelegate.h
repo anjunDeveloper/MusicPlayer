c90aa8b63e5c336ba9a533d77c559ab68491dfee
安俊(平安科技智慧生活团队) <ANJUN604@pingan.com.cn>
2019-03-26 15:21:04 +0800

    Initial Commit

---
+++ MusicPlayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MusicPlayerDemo
+//
+//  Created by 安俊(平安科技智慧生活团队) on 2019/3/26.
+//  Copyright © 2019年 安俊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MusicPlayerDemoTests/MusicPlayerDemoTests.m
@@ -0,0 +1,37 @@
+//
+//  MusicPlayerDemoTests.m
+//  MusicPlayerDemoTests
+//
+//  Created by 安俊(平安科技智慧生活团队) on 2019/3/26.
+//  Copyright © 2019年 安俊. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MusicPlayerDemoTests : XCTestCase
+
+@end
+
+@implementation MusicPlayerDemoTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
